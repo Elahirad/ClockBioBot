9e5b82737f4a8d66ab534df5af8e0ab2c13746df
NYXTheShadow <alielahirad8087@gmail.com>
2022-01-20 01:43:17 +0330

    Preparing for Heroku.

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ from pyrogram.errors import FloodWait
 import datetime
 import asyncio
 import os
-from dotenv import load_dotenv
-load_dotenv()
 # Getting API ID and API HASH
 api_id = os.environ.get("API_ID")
 api_hash = os.environ.get("API_HASH")
 bio_string = os.environ.get("BIO_STR")
 session_name = os.environ.get("SESSION_NAME")
-print(api_id)
-print(api_hash)
-print(bio_string)
-print(session_name)
+print("Working with API ID : " + api_id)
+print("Working with API HASH : " + api_hash)
+print("Bio String is : " + bio_string)
+print("Working with SESSION NAME : " + session_name)
 
-
+# Doeing Works
 async def main():
     async with Client(api_id=api_id, api_hash=api_hash, session_name=session_name) as BioBotClient:
         try:
